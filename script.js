8997cb28a11e4df04ac64786c7f11dcc3c1a7fbb
Katiacrisci <katcrisci@gmail.com>
2023-11-13 18:18:14 +0100

    fix javaScript

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const closeButton = document.querySelector(".fas.fa-times").parentElement;
 const sideBarButton = document.createElement("button");
+sideBarButton.id = "sidebarButton"
 const centralColumn = document.querySelector(".container-fluid .row .col-7");
 const rightColumn = document.querySelector(".container-fluid .row .col-2");
-sideBarButton.style.display = "none"
+sideBarButton.classList.add("d-none");
 sideBarButton.onclick = () => {
     rightColumn.classList.remove("d-none");
     centralColumn.classList.remove("col-9");
-    sideBarButton.classList.add("d-none")
-    
+    sideBarButton.classList.add("d-none");
+
 }
 
 closeButton.onclick = () => {
    
     rightColumn.classList.add("d-none");
     centralColumn.classList.add("col-9");
-    sideBarButton.style.display = "unset"
+    sideBarButton.classList.remove("d-none")
     centralColumn.appendChild(sideBarButton);
     
     
