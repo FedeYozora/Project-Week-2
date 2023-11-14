cfb50b01eb8b1cbe59617bf08eca4f4fef39b663
Katiacrisci <katcrisci@gmail.com>
2023-11-14 10:07:35 +0100

    add sidebarbutton

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 const closeButton = document.querySelector(".fas.fa-times").parentElement;
-const sideBarButton = document.createElement("button");
-sideBarButton.id = "sidebarButton"
 const centralColumn = document.querySelector(".container-fluid .row .col-7");
 const rightColumn = document.querySelector(".container-fluid .row .col-2");
-sideBarButton.classList.add("d-none");
+const sideBarButton = document.getElementById("sidebarButton");
 sideBarButton.onclick = () => {
-    rightColumn.classList.remove("d-none");
-    centralColumn.classList.remove("col-9");
-    sideBarButton.classList.add("d-none");
-
-}
+  rightColumn.classList.remove("d-none");
+  centralColumn.classList.remove("col-9");
+};
 
 closeButton.onclick = () => {
-   
-    rightColumn.classList.add("d-none");
-    centralColumn.classList.add("col-9");
-    sideBarButton.classList.remove("d-none")
-    centralColumn.appendChild(sideBarButton);
-    
-    
-   
-}
+  rightColumn.classList.add("d-none");
+  centralColumn.classList.add("col-9");
+  sideBarButton.classList.remove("d-none");
+};
